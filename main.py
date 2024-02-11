62f79989a7c8941caaa1dac54f15898e9befe462
Harry Hebden <hazahebden@gmail.com>
2024-02-11 15:45:53 +1300

    Cut out Thought class into own module/file.

--- main.py
+++ main.py
@@ -21,11 +21,14 @@ from PySide6.QtWidgets import (
     QGraphicsProxyWidget, QGraphicsWidget,
 )
 
+
+from thought import GraphicsThought
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setGeometry(2000, 50, 900, 700)
-        self.setWindowTitle("Let's explore this!")
+        self.setWindowTitle("Follow that Thought!")
         graph = GraphWidget(self)
         self.setCentralWidget(graph)
 
@@ -63,41 +66,6 @@ class GraphWidget(QGraphicsView):
     #     print(f"{self.itemAt(pos_x, pos_y)=}")
 
     #     return super().mousePressEvent(event)
-
-class Thought(QWidget):
-    def __init__(self, left, top, width, height) -> None:
-        super().__init__()
-        v_layout = QVBoxLayout()
-
-        self.text_area = QTextEdit()
-        self.text_area.setBaseSize(300, 300)
-        self.text_area.setStyleSheet("background-color: black; border-style: solid; border-width:1px; border-radius:50px; border-color: red;")
-
-        v_layout.addWidget(self.text_area)
-
-        self.add_button = QPushButton("+")
-
-        v_layout.addWidget(self.add_button)
-
-        self.setLayout(v_layout)
-
-# Dev 2
-class GraphicsThought(QGraphicsRectItem):
-    def __init__(self, left, top, width, height, margin = 10) -> None:
-        super().__init__(QRect(left-margin, top-margin, width+(2*margin), height+(2*margin)))
-
-        base_widget = Thought(left, top, height, width)
-        base_widget.setGeometry(QRect(left, top, height, width))
-
-        # graphics_widget = QGraphicsRectItem(QRect(0,0,120,145))
-        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable | QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
-        self.setBrush(Qt.GlobalColor.red)
-
-        proxy = QGraphicsProxyWidget()
-        proxy.setWidget(base_widget)
-        proxy.setParentItem(self)
-
-
 
 
 if __name__ == "__main__":
---
+++ thought.py
@@ -0,0 +1,54 @@
+import sys
+from typing import Optional
+
+from PySide6.QtCore import (
+    Qt, QEvent, QLineF, QRect,
+    QPointF, QPoint, QRectF
+)
+from PySide6.QtGui import (
+    QPainter, QPaintEvent, QFont,
+    QTransform, QColor, QMouseEvent,
+    QPolygonF, QPolygon, QRegion,
+)
+
+from PySide6.QtWidgets import (
+    QApplication, QWidget, QMainWindow,
+    QGraphicsView, QGraphicsScene,
+    QGraphicsItem, QVBoxLayout,
+    QHBoxLayout, QGraphicsRectItem,
+    QLineEdit, QTextEdit,
+    QPushButton, QFrame,
+    QGraphicsProxyWidget, QGraphicsWidget,
+)
+
+class Thought(QWidget):
+    def __init__(self, left, top, width, height) -> None:
+        super().__init__()
+        v_layout = QVBoxLayout()
+
+        self.text_area = QTextEdit()
+        self.text_area.setBaseSize(300, 300)
+        self.text_area.setStyleSheet("background-color: black; border-style: solid; border-width:1px; border-radius:50px; border-color: red;")
+
+        v_layout.addWidget(self.text_area)
+
+        self.add_button = QPushButton("+")
+
+        v_layout.addWidget(self.add_button)
+
+        self.setLayout(v_layout)
+
+class GraphicsThought(QGraphicsRectItem):
+    def __init__(self, left, top, width, height, margin = 10) -> None:
+        super().__init__(QRect(left-margin, top-margin, width+(2*margin), height+(2*margin)))
+
+        base_widget = Thought(left, top, height, width)
+        base_widget.setGeometry(QRect(left, top, height, width))
+
+        # graphics_widget = QGraphicsRectItem(QRect(0,0,120,145))
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable | QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
+        self.setBrush(Qt.GlobalColor.red)
+
+        proxy = QGraphicsProxyWidget()
+        proxy.setWidget(base_widget)
+        proxy.setParentItem(self)
